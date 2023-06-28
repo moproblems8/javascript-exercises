1e85fab7649358ef6c16d689022d8bcef879da43
moproblems8 <mhasan1080@gmail.com>
2023-06-27 17:48:33 -0700

    passed all test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < args.length; i++) { 
+        if (arr.includes(args[i])) {
+            arr.splice(arr.indexOf(args[i]), 1);
+        }   
+    }
+    return arr;
 };
 
 // Do not edit below this line
