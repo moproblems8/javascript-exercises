8d7e3d98cd7a8f0282f918e3a63428389f852c99
moproblems8 <mhasan1080@gmail.com>
2023-06-27 17:34:48 -0700

    3rd exercise passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    word = word.split(''); // split word into array of letters
+    word = word.reverse(); // reverse array of letters
+    word = word.join(''); // join array of letters into word
 
+    return word;
 };
 
 // Do not edit below this line
